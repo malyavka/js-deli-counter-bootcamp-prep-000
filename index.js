60d81688ab1544776b6eda07be5b49da87511a98
Maria Obaturova <m.obaturova@gmail.com>
2018-12-04 19:57:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(currentLine, name) {
+  currentLine.push(name);
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
+  
+}
+
+
+function nowServing(katzDeliLine) {
+  if (!katzDeliLine.length) {
+    return "There is nobody waiting to be served!";
+  }
+}
